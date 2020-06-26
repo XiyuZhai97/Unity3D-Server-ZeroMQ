a8c35cab1fe91b8b1c0aa0d11d8855d7827f22a7
XiyuZhai <zxy4970@gmail.com>
2020-06-25 18:59:52 -0700

    build basic communication

---
+++ Client-Unity/Assets/Scripts/HelloClient.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HelloClient : MonoBehaviour
+{
+    public Text Receiving;
+    public Text Sending;
+    public Text Status;
+    public Button stopConnect;
+    public Button startConnect;
+    public InputField ipInput;
+    public InputField roomNameInput;
+
+    private HelloRequester _helloRequester;
+
+    private void Start()
+    {
+        // _helloRequester = new HelloRequester();
+        Status.text = "Stopped";
+        stopConnect.onClick.AddListener(stopConnectFun);
+        startConnect.onClick.AddListener(startConnectFun);
+
+    }
+    void Update()
+    {
+        if(_helloRequester != null){
+
+            Receiving.text = _helloRequester.received;
+            Sending.text = _helloRequester.sended;
+        }
+        // Status.text = _helloRequester.status;
+    }
+
+    void startConnectFun()
+    {
+        
+        Status.text = "Stop Connect ...";
+        if(_helloRequester != null){
+            _helloRequester.Stop();
+        }
+
+        Status.text = "Start Connect ...";
+        _helloRequester = new HelloRequester();
+        _helloRequester.serverIP = ipInput.text;
+        _helloRequester.Start();
+    }
+    void stopConnectFun()
+    {
+        Status.text = "Stop Connect ...";
+        _helloRequester.Stop();
+    }
+    private void OnDestroy()
+    {
+        _helloRequester.Stop();
+    }
+}
